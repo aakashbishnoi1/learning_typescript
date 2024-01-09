e8c97495af4c9a8459257bdbc811b670b89a50d7
aakashbishnoi1 <8xaakash@gmail.com>
2024-01-09 14:45:31 +0530

    literal types in ts
    we can have literal string or numeric or boolean values as types

---
+++ literal_types.ts
@@ -0,0 +1,23 @@
+// we can have literal values as our types 
+
+// String literals
+let color: "red" | "black" | "blue";
+
+color = "red";
+color = "black";
+// Error
+// color = "White";
+
+// numeric literals 
+let num: 1 | 2 | 3;
+
+num = 2;
+//Error 
+//num = 4;
+
+// Boolean literals
+let bool: true;
+
+bool = true;
+//Error 
+// bool = false;
