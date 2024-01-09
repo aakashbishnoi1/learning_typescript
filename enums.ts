cdcb82d1dc2272a0870afa5370cba6d58ca65149
aakashbishnoi1 <8xaakash@gmail.com>
2024-01-09 15:04:23 +0530

    enums in ts

---
+++ enums.ts
@@ -0,0 +1,10 @@
+// values are by default incremented from 0 if not defined
+enum WeatherConditions {
+    sunny = "sunny",
+    cloudy = "cloudy",
+    rainy = "rainy",
+    snowy = "snowy",
+}
+
+let currentWeather = WeatherConditions.sunny;
+console.log(currentWeather);
